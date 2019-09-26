a46e380897f6ab7f75b90a3a591a868474af6945
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-09-26 09:52:31 -0400

    added to do steps

--- src/com/lickingheights/Main.java
+++ src/com/lickingheights/Main.java
@@ -3,6 +3,37 @@ package com.lickingheights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// todo declare variables a, b, c,discriminate, axisOFtsymmetry, patrabloaFacing, x and y
+
+   // todo initialize ammanuly
+
+   // todo do calculations
+
+   // todo print results
+
+   // todo not in code; check results
+
+   // todo initialize scanner and redo initialize area
+
+   // todo double check
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
