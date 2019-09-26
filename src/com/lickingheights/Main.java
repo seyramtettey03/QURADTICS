11dac35342e74a3f59cf0075a6323ef9cb85cc35
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-09-26 09:58:26 -0400

    declaring variables

--- src/com/lickingheights/Main.java
+++ src/com/lickingheights/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// todo declare variables a, b, c,discriminate, axisOFtsymmetry, patrabloaFacing, x and y
-
+     double  a,b,c,descriminate,axisoftsymmetry,x,y;
+     String parabloafacing;
    // todo initialize ammanuly
 
    // todo do calculations
